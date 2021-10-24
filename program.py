bcbba14e571307062b2dace34f75a3efacc56819
python290821 <90146835+python290821@users.noreply.github.com>
2021-10-24 20:02:38 +0300

    Update program.py

--- program.py
+++ program.py
@@ -1,12 +1,12 @@
 
-# 1
+################################### 1
 # input user first and last name
 # i.e. moshe cohen
 # if the user ented more then 2 words
 # i.e. cohen levi david , then input again
 # until 2 words were given
 # then print first name and last name in seperated lines
-
+################################### 2
 # input a string from the user (hint: use replace and find...)
 # print:
 # i.e. '123' -- int
@@ -14,4 +14,4 @@
 # i.e. '5.7.1' -- illegal
 # i.e. ' 123' -- int
 # i.e. 'a' -- alpha (isaplha)
-# i.e. 'a1' -- illegal
+# i.e. 'a1' -- mix
