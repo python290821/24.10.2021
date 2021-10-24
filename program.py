0fc1dc882a31335ff9b6adbc32813b1ac721fd17
python290821 <90146835+python290821@users.noreply.github.com>
2021-10-24 17:55:49 +0300

    Update program.py

--- program.py
+++ program.py
@@ -1,6 +1,9 @@
 
-def get_in_range(_min, _max):
+def get_in_range(_min = 1, _max = 100):
     while True:
         number = int(input(f'please enter number in range({_min}-{_max}'))
         if number >= _min and number <= _max:
             return number
+
+x = get_in_range(10)
+x = get_in_range(1, 10)
